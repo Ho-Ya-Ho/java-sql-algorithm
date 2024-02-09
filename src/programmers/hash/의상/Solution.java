1d5286a385cb2921f4eaf9e0bf2df98ace266798
Ho-Ya-Ho <rkdgh1056@gmail.com>
2024-02-09 23:08:05 +0900

    hash - 의상

--- src/programmers/hash/의상/Solution.java
+++ src/programmers/hash/의상/Solution.java
@@ -9,20 +9,15 @@ import java.util.HashMap;
  */
 class Solution {
 	public int solution(String[][] clothes) {
-		int answer = 0;
 
 		// 옷 종류 별로 hashMap 에 데이터 삽입
 		HashMap<String, Integer> hashMap = new HashMap<>();
 		Arrays.stream(clothes).forEach(clothe -> hashMap.put(clothe[1], hashMap.getOrDefault(clothe[1], 0) + 1));
 
-		Integer count = hashMap.values().stream().reduce((a, b) -> a * b).get();
+		int count = hashMap.values().stream()
+				.mapToInt(value -> value + 1)
+				.reduce(1, (a, b) -> a * b);
 
-		if (hashMap.size() == 1) {
-			answer = clothes.length;
-		} else {
-			answer = count + clothes.length;
-		}
-
-		return answer;
+		return count - 1;
 	}
 }
