98aae7be57228ebc54286bc32cad213e15b7b6eb
Ho-Ya-Ho <rkdgh1056@gmail.com>
2024-02-13 17:30:24 +0900

    hash - 베스트앨범-미완료

--- src/programmers/hash/베스트앨범/Solution.java
+++ src/programmers/hash/베스트앨범/Solution.java
@@ -1,55 +1,40 @@
 package programmers.hash.베스트앨범;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * 1. 각 장르별로 HashMap 에 넣어서 각 장르별 우선순위 체크
+ * 2. 각 장르별 sort stream 돌면서 최상위 2개 반환
+ * 3.
+ */
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
 
-        HashMap<String, Song> hashMap = new HashMap<>();
+        HashMap<String, List<Integer>> hashMap = new HashMap<>();
 
+		// 값 넣기
         for (int i = 0; i < genres.length; i++) {
-            hashMap.put(genres[i], new Song(plays[i], i));
+			List<Integer> list = hashMap.getOrDefault(genres[i], new ArrayList<>());
+	        list.add(plays[i]);
+	        hashMap.put(genres[i], list);
         }
 
-        System.out.println("hashMap = " + hashMap);
+		// 정렬
+	    hashMap.values().forEach(list -> list.sort(Collections.reverseOrder()));
+
+		// 장르별 합이
+	    HashMap<String, Integer> sumHashMap = new HashMap<>();
+		hashMap.forEach((key, value) -> sumHashMap.put(key, value.stream().reduce(Integer::sum).get()));
+
+
+
+	    System.out.println("hashMap = " + hashMap);
+	    System.out.println("sumHashMap = " + sumHashMap);
 
         return answer;
     }
 
-
-    public static class Song {
-        private int play;
-        private int primaryKey;
-
-        public Song(int play, int primaryKey) {
-            this.play = play;
-            this.primaryKey = primaryKey;
-        }
-
-        @Override
-        public String toString() {
-            return "Song{" +
-                    "play=" + play +
-                    ", primaryKey=" + primaryKey +
-                    '}';
-        }
-
-        public int getPlay() {
-            return play;
-        }
-
-        public void setPlay(int play) {
-            this.play = play;
-        }
-
-        public int getPrimaryKey() {
-            return primaryKey;
-        }
-
-        public void setPrimaryKey(int primaryKey) {
-            this.primaryKey = primaryKey;
-        }
-    }
 }
 
