fc2ab502af91661cdfeb1de0e4afd94a82e48731
Ho-Ya-Ho <rkdgh1056@gmail.com>
2024-02-14 22:02:10 +0900

    hash - 베스트앨범

--- src/programmers/hash/베스트앨범/Solution.java
+++ src/programmers/hash/베스트앨범/Solution.java
@@ -3,38 +3,47 @@ package programmers.hash.베스트앨범;
 import java.util.*;
 import java.util.stream.Collectors;
 
-/**
- * 1. 각 장르별로 HashMap 에 넣어서 각 장르별 우선순위 체크
- * 2. 각 장르별 sort stream 돌면서 최상위 2개 반환
- * 3.
- */
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        List<Integer> answer = new ArrayList<>();
+		HashMap<String, Integer> sumHashMap = new HashMap<>();
 
-        HashMap<String, List<Integer>> hashMap = new HashMap<>();
-
-		// 값 넣기
+		// 합 결과를 가지는 hashMap 생성
         for (int i = 0; i < genres.length; i++) {
-			List<Integer> list = hashMap.getOrDefault(genres[i], new ArrayList<>());
-	        list.add(plays[i]);
-	        hashMap.put(genres[i], list);
+			sumHashMap.put(genres[i], sumHashMap.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-		// 정렬
-	    hashMap.values().forEach(list -> list.sort(Collections.reverseOrder()));
+		// 합 결과를 정렬한 keyList 생성
+        ArrayList<String> keyList = new ArrayList<>(sumHashMap.keySet());
+		keyList.sort(((o1, o2) -> sumHashMap.get(o2) - sumHashMap.get(o1)));
 
-		// 장르별 합이
-	    HashMap<String, Integer> sumHashMap = new HashMap<>();
-		hashMap.forEach((key, value) -> sumHashMap.put(key, value.stream().reduce(Integer::sum).get()));
+		// 정렬된 장르 순서대로 반복문 돌면서 개수만큼 추출
+		for (String key : keyList) {
+			ArrayList<Song> songList = new ArrayList<>();
+			for (int i = 0; i < genres.length; i++) {
+				if (key.equals(genres[i])) {
+					songList.add(new Song(plays[i], i));
+				}
+			}
 
+			List<Song> collect = songList.stream().sorted((s1, s2) -> Integer.compare(s2.play, s1.play)).collect(Collectors.toList());
 
+			answer.add(collect.get(0).primaryKey);
+			if (songList.size() != 1) {
+				answer.add(collect.get(1).primaryKey);
+			}
+		}
 
-	    System.out.println("hashMap = " + hashMap);
-	    System.out.println("sumHashMap = " + sumHashMap);
-
-        return answer;
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 
-}
+	public static class Song {
+		int play;
+		int primaryKey;
 
+		public Song(int play, int primaryKey) {
+			this.play = play;
+			this.primaryKey = primaryKey;
+		}
+	}
+}
