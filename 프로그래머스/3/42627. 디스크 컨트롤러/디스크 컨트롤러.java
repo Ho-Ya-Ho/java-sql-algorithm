b30115096d1a51bff52b4611b00cab8dc3cf0495
김강호 <87962611+Ho-Ya-Ho@users.noreply.github.com>
2024-02-23 01:45:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.45 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,59 @@
+import java.util.PriorityQueue;
+
+class Solution {
+
+    private class Job {
+        private final int start;
+        private final int cost;
+
+        public Job(int start, int cost) { 
+            this.start = start;
+            this.cost = cost;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getCost() {
+            return cost;
+        }
+    }
+
+    public int solution(int[][] jobs) {
+        int count = 0;
+        int waitTime = 0; // 다른 CPU를 기다리는 시간
+        int costTime = 0;
+        int idle = 0; // CPU 가 어떠한 작업도 하지 않을 때 다음 CPU 사용을 기다리는 시간
+
+        PriorityQueue<Job> wait = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
+        PriorityQueue<Job> ready = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+
+        //wait queue 값 세팅
+        for (int[] job : jobs) {
+            wait.add(new Job(job[0], job[1]));
+        }
+
+        while (count < jobs.length) {
+            while (!wait.isEmpty() && wait.peek().getStart() <= costTime) {
+                ready.add(wait.poll());
+            }
+
+            if (!ready.isEmpty()) {
+                Job job = ready.poll();
+                waitTime += costTime - job.getStart();
+                costTime += job.getCost();
+                count++;
+            } else {
+                idle += wait.peek().getStart() - costTime;
+                costTime = wait.peek().getStart();
+            }
+        }
+
+        return getAnswer(waitTime, costTime, idle, jobs.length);
+    }
+
+    public int getAnswer(int waitTime, int costTime, int idle, int jobsLength) {
+        return (waitTime + costTime - idle) / jobsLength;
+    }
+}
