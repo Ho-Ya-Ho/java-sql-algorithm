f96ea9766969bb8c6ce5616b431e94a77dd76b47
김강호 <87962611+Ho-Ya-Ho@users.noreply.github.com>
2024-09-12 22:46:43 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
