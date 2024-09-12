8fb3e9d643ae7aab3e73e4af923d267951afe3eb
김강호 <87962611+Ho-Ya-Ho@users.noreply.github.com>
2024-09-12 21:42:59 +0900

    [Bronze V] Title: 개, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+        bw.flush();
+        bw.close();
+    }
+}
